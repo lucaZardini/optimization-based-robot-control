713c9f3b0a725dffdf94601981f502e04a57f30f
lucaZardini <luca.zardini@studenti.unitn.it>
2022-10-27 16:18:00 +0200

    Refactor of the code

--- 01_assignment/hw1_conf.py
+++ 01_assignment/hw1_conf.py
@@ -16,10 +16,10 @@ LINE_WIDTH = 60
 DATA_FILE_LIPM = 'talos_walking_traj_lipm.npz'
 DATA_FILE_TSID = 'talos_walking_traj_tsid.npz'
 
-PUSH = 0                                               # Flag to activate CoM push @ half walk
-push_robot_com_vel = [0.1,0.,0.]                       # CoM velocity push
-SQUAT = 0                                              # Flag to activate squat task
-squat_height = 0.65                                    # desired CoM height while squatting 
+PUSH = 1                                               # Flag to activate CoM push @ half walk
+push_robot_com_vel = [0.0,0.,-0.5]                       # CoM velocity push
+SQUAT = 1                                              # Flag to activate squat task
+squat_height = 0.65                                    # desired CoM height while squatting
 
 # robot parameters
 # ----------------------------------------------
@@ -63,14 +63,14 @@ dt = 0.002                      # controller time step
 T_pre  = 1.5                    # simulation time before starting to walk
 T_post = 0                      # simulation time after walking
 
-w_com = 5e-1                    # weight of center of mass task (ref trajectory)
+w_com = 100                # weight of center of mass task (ref trajectory)
 if SQUAT:
-    w_squat = 10                # weight of squat task
+    w_squat = 100              # weight of squat task
 else:
-    w_squat = 0                 # weight of squat task
-w_foot = 1e-1                   # weight of the foot motion task
+    w_squat = 100                # weight of squat task
+w_foot = 1e-1                  # weight of the foot motion task
 w_contact = 1e2                 # weight of the foot in contact
-w_posture = 2e-2                # weight of joint posture task
+w_posture = 3e-4                # weight of joint posture task
 w_forceRef = 1e-5               # weight of force regularization task
 w_torque_bounds = 0.0           # weight of the torque bounds
 w_joint_bounds = 0.0            # weight of the joint bounds
@@ -86,8 +86,8 @@ if PUSH:
     kp_com = 100.0              # proportional gain of center of mass task (ref trajectory)
     kp_squat = 100.0            # proportional gain of squat task
 else:
-    kp_com = 10.0              # proportional gain of center of mass task (ref trajectory)
-    kp_squat = 10.0            # proportional gain of squat task
+    kp_com = 100.0              # proportional gain of center of mass task (ref trajectory)
+    kp_squat = 100.0            # proportional gain of squat task
 kp_posture = 1.0               # proportional gain of joint posture task
 kp_am = 10.0                   # proportional gain of angular momentum task
 
