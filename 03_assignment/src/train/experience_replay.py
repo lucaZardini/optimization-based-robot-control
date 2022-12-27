197f40685a2bdf46094cbef1ce7cdc9c633e8e03
lucaZardini <luca.zardini@studenti.unitn.it>
2022-12-27 12:16:31 +0100

    Implemented the experience replay class

--- 03_assignment/src/train/experience_replay.py
+++ 03_assignment/src/train/experience_replay.py
@@ -1,7 +1,19 @@
+import random
 
 
 class ExperienceReplay:
-    pass
 
-    def append(self):
-        pass
+    def __init__(self, buffer_size: int, batch_size: int):
+        self.buffer: list = []
+        self.buffer_size = buffer_size
+        self.batch_size = batch_size
+
+    def append(self, transition):  # TODO: typing
+        if len(self.buffer) == self.buffer_size:
+            self.buffer.pop(0)
+        self.buffer.append(transition)
+
+    def sample_random_minibatch(self):
+        if len(self.buffer) < self.batch_size:
+            raise IndexError(f"The size of the buffer ({len(self.buffer)}) is lower than the batch size ({self.batch_size})")
+        return random.sample(self.buffer, self.batch_size)
